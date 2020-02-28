610f6b16d1ca457a79e6593aaa6e50e029914523
木大酱 <west3316@qq.com>
2020-02-28 17:17:53 +0800

    调整播放页面

--- www/js/play.js
+++ www/js/play.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function (event) {
                 }
             ]
         },
-        "width": 800,
-        "height": 522
+        "width": 1000,
+        "height": 690
     })
 })
 
@@ -22,6 +22,12 @@ new Vue({
     data: {
         playlist: [
             { 'id': 1, 'title': '两只黄鹂鸣翠柳', 'active': true },
+            { 'id': 2, 'title': '一行白鹭上青天', 'active': false },
+            { 'id': 3, 'title': '窗含西岭千秋雪', 'active': false },
+            { 'id': 4, 'title': '门泊东吴万里船', 'active': false },
+        ],
+        danmulist: [
+            { 'id': 1, 'title': '两只黄鹂鸣翠柳', 'active': false },
             { 'id': 2, 'title': '一行白鹭上青天', 'active': false },
             { 'id': 3, 'title': '窗含西岭千秋雪', 'active': false },
             { 'id': 4, 'title': '门泊东吴万里船', 'active': false },
