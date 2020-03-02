be6c4a731fb308e8a640352fbb9e034d7222ddc4
木大酱 <west3316@qq.com>
2020-03-02 21:58:01 +0800

    fix ddl

--- model/sql/build_db.sql
+++ model/sql/build_db.sql
@@ -1,9 +1,7 @@
 CREATE SCHEMA `study_ground` DEFAULT CHARACTER SET utf8mb4 ;
 
-ALTER TABLE `study_ground`.`users` AUTO_INCREMENT = 100001;
-
 CREATE TABLE `study_ground`.`users` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `email` VARCHAR(45) NOT NULL COMMENT '邮箱地址',
   `password` VARCHAR(64) NOT NULL COMMENT '密码',
   `created_at` DATETIME NOT NULL COMMENT '注册时间',
@@ -12,3 +10,5 @@ CREATE TABLE `study_ground`.`users` (
   `avatar` VARCHAR(128) NULL COMMENT '头像',
   PRIMARY KEY (`id`),
   UNIQUE INDEX `unique` (`email` ASC)) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `study_ground`.`users` AUTO_INCREMENT = 100001;
